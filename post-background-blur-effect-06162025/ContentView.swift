02882dbc219fc3e5b324921db0eb39997ee2d2ef
DaveChan-ux <david.chan@rewardstyle.com>
2025-06-16 10:35:32 -0700

    Initial Commit

---
+++ post-background-blur-effect-06162025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  post-background-blur-effect-06162025
+//
+//  Created by Dave.Chan on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ post-background-blur-effect-06162025/post_background_blur_effect_06162025App.swift
@@ -0,0 +1,17 @@
+//
+//  post_background_blur_effect_06162025App.swift
+//  post-background-blur-effect-06162025
+//
+//  Created by Dave.Chan on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct post_background_blur_effect_06162025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
